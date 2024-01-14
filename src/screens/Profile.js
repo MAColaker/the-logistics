353c8b8d14124910084742f14ee0169bdacc1340
Muhammed Ali ÇOLAKER <m.alicolaker@gmail.com>
2024-01-14 11:02:56 +0300

    add claims control

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {ListItem, Separator, YGroup, YStack} from 'tamagui';
 
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {useNavigation} from '@react-navigation/native';
+
+import auth from '@react-native-firebase/auth';
 
 import MyJobs from './MyJobs';
 import Jobs from './Jobs';
@@ -15,8 +17,18 @@ const ProfileStack = createNativeStackNavigator();
 
 const ProfileScreen = () => {
   const {left, top, right} = useSafeAreaInsets();
+  const [claims, setClaims] = useState([]);
 
   const navigation = useNavigation();
+
+  useEffect(() => {
+    const getClaims = async () => {
+      const idTokenResult = await auth().currentUser.getIdTokenResult();
+      setClaims(idTokenResult.claims);
+    };
+
+    getClaims();
+  }, []);
 
   return (
     <YStack top={top} left={left} right={right} fullscreen space>
@@ -36,39 +48,43 @@ const ProfileScreen = () => {
           />
         </YGroup.Item>
       </YGroup>
-      <YGroup
-        alignSelf="center"
-        bordered
-        size="$5"
-        separator={<Separator />}
-        marginHorizontal={21}>
-        <YGroup.Item>
-          <ListItem
-            hoverTheme
-            pressTheme
-            title="Alınan İşler"
-            subTitle="Şöförlerin aldığı işler"
-            onPress={() => navigation.navigate('Jobs')}
-          />
-        </YGroup.Item>
-        <YGroup.Item>
-          <ListItem
-            hoverTheme
-            pressTheme
-            title="İş ekle"
-            subTitle="Yeni iş girişi yap"
-            onPress={() => navigation.navigate('AddJobs')}
-          />
-        </YGroup.Item>
-        <YGroup.Item>
-          <ListItem
-            hoverTheme
-            pressTheme
-            title="Üyeler"
-            onPress={() => navigation.navigate('Users')}
-          />
-        </YGroup.Item>
-      </YGroup>
+      {!!claims.admin ? (
+        <YGroup
+          alignSelf="center"
+          bordered
+          size="$5"
+          separator={<Separator />}
+          marginHorizontal={21}>
+          <YGroup.Item>
+            <ListItem
+              hoverTheme
+              pressTheme
+              title="Alınan İşler"
+              subTitle="Şöförlerin aldığı işler"
+              onPress={() => navigation.navigate('Jobs')}
+            />
+          </YGroup.Item>
+          <YGroup.Item>
+            <ListItem
+              hoverTheme
+              pressTheme
+              title="İş ekle"
+              subTitle="Yeni iş girişi yap"
+              onPress={() => navigation.navigate('AddJobs')}
+            />
+          </YGroup.Item>
+          <YGroup.Item>
+            <ListItem
+              hoverTheme
+              pressTheme
+              title="Üyeler"
+              onPress={() => navigation.navigate('Users')}
+            />
+          </YGroup.Item>
+        </YGroup>
+      ) : (
+        <></>
+      )}
     </YStack>
   );
 };
